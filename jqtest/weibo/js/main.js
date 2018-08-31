a6843b67ec2004109c6ceff8d7addb2f037a1471
santana2000 <532014809@qq.com>
2018-08-31 19:55:20 +0800

    webstorm link

--- jqtest/weibo/js/main.js
+++ jqtest/weibo/js/main.js
@@ -2,12 +2,46 @@
 
 $(function () {
     //1.输入内容后才能点击发布按钮
-        $('.release').attr('disabled',false);
+
+    $('.release').attr('disabled',false);
 
 
 
 
 
+    $('.release').click(function () {
+
+        var words = $('.write').val();
+
+        var  x = $('<div class="content">\n' +
+            '            <div class="main-word">\n' +
+                              words+
+            '            </div>\n' +
+            '            <div class="commit">\n' +
+            '                <span class="time">'+nowTime()+'</span>\n' +
+            '                <span class="zan">删除</span>\n' +
+            '            </div>\n' +
+            '        </div>');
+
+        function nowTime() {
+            var date = new Date();
+            var a = date.getFullYear();
+            var b = date.getMonth()+1;
+            var c = date.getDate();
+            var today = a+'-'+b+'-'+c;
+            return today
+
+        }
+
+        $('.center').append(x);
+        console.log('hh.log');
+    });
+
+
+    $('.center').delegate('.zan','click',function () {
+
+        $(this).parents('.content').remove();
+    })
 
 
 
